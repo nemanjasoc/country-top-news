66d5aa1bb2f98ff13ff64cc45c13976f4a7c621f
nemanjasoc <nemanjasoc@gmail.com>
2020-05-06 13:36:49 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatListModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
